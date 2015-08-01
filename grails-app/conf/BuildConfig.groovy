84c8b850fc2fb7b7f99d275cc4a13e3f742f487b
dularion <antonia.hulha@systaro.de>
2015-08-01 17:28:40 +0200

    removed duplicate line

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,4 @@
 grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
-grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
