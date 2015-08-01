33a81ed971b0fc192a9b8e1bff91da1d4adddb8e
Christian Gärtner <chrisdagardner@gmail.com>
2015-08-02 00:28:19 +0200

    Fixed test suite and added a basic unit test for Episode
    this is very basic but serves as a proof of concept. grailsw test-app works now!
    
    The unit test thing is passing, but it fails during verifying some sql stuff,
    needs investigation

--- test/unit/streama/EpisodeSpec.groovy
+++ test/unit/streama/EpisodeSpec.groovy
@@ -1,20 +1,26 @@
 package streama
-
 import grails.test.mixin.TestFor
 import spock.lang.Specification
-
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Episode)
 class EpisodeSpec extends Specification {
 
-    def setup() {
-    }
+    void "test episode naming"() {
+      setup:
+        def episode0 = Episode.newInstance()
+        def episode1 = Episode.newInstance()
+        episode0.season_number = 1
+        episode0.episode_number = 33
+        episode0.beforeUpdate()
 
-    def cleanup() {
-    }
+        episode1.season_number = 640
+        episode1.episode_number = 2
+        episode1.beforeUpdate()
 
-    void "test something"() {
+      expect:
+        episode0.episodeString == "s01e33"
+        episode1.episodeString == "s640e02"
     }
 }
