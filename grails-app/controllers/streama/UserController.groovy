213fc2c0e330a9845d45a6ebde9e6ff61574efeb
dularion <antonia.hulha@systaro.de>
2015-08-02 10:50:29 +0200

    formatting

--- grails-app/controllers/streama/UserController.groovy
+++ grails-app/controllers/streama/UserController.groovy
@@ -7,147 +7,148 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class UserController {
 
-    def validationService
-    def springSecurityService
+  def validationService
+  def springSecurityService
 
-    static responseFormats = ['json', 'xml']
-    static allowedMethods = [save: "POST", delete: "DELETE"]
+  static responseFormats = ['json', 'xml']
+  static allowedMethods = [save: "POST", delete: "DELETE"]
 
-    def index() {
-        respond User.findAllByDeletedNotEqual(true), [status: OK]
+  def index() {
+    respond User.findAllByDeletedNotEqual(true), [status: OK]
+  }
+
+  @Transactional
+  def save(User userInstance) {
+    if (userInstance == null) {
+      render status: NOT_FOUND
+      return
     }
 
-    @Transactional
-    def save(User userInstance) {
-        if (userInstance == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        userInstance.validate()
-        if (userInstance.hasErrors()) {
-            render status: NOT_ACCEPTABLE
-            return
-        }
-
-        userInstance.save flush:true
-        respond userInstance, [status: CREATED]
+    userInstance.validate()
+    if (userInstance.hasErrors()) {
+      render status: NOT_ACCEPTABLE
+      return
     }
 
-    def show(User userInstance){
-        respond userInstance, [status: OK]
+    userInstance.save flush: true
+    respond userInstance, [status: CREATED]
+  }
+
+  def show(User userInstance) {
+    respond userInstance, [status: OK]
+  }
+
+  @Transactional
+  def delete(User userInstance) {
+
+    if (userInstance == null) {
+      render status: NOT_FOUND
+      return
     }
 
-    @Transactional
-    def delete(User userInstance) {
+    userInstance.deleted = true
+    userInstance.accountExpired = true
 
-        if (userInstance == null) {
-            render status: NOT_FOUND
-            return
-        }
+    userInstance.save flush: true, failOnError: true
 
-        userInstance.deleted = true
-        userInstance.accountExpired = true
+    render status: NO_CONTENT
+  }
 
-        userInstance.save flush: true, failOnError: true
+  def checkAvailability() {
+    def username = params.username
+    def result = [:]
 
-        render status: NO_CONTENT
+    if (User.findByUsername(username)) {
+      result.error = "User with that E-Mail-Address already exists."
     }
 
-    def checkAvailability() {
-        def username = params.username
-        def result = [:]
+    respond result
+  }
 
-        if(User.findByUsername(username)){
-            result.error = "User with that E-Mail-Address already exists."
-        }
+  @Transactional
+  def saveAndInviteUser() {
 
-        respond result
+    def data = request.JSON
+
+    User userInstance = User.findOrCreateById(data.id)
+
+    if (userInstance == null) {
+      render status: NOT_FOUND
+      return
     }
 
-    @Transactional
-    def saveAndInviteUser() {
+    userInstance.properties = data
 
-        def data = request.JSON
-
-        User userInstance = User.findOrCreateById(data.id)
-
-        if (userInstance == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        userInstance.properties = data
-
-        userInstance.validate()
-        if (userInstance.hasErrors()) {
-            render status: NOT_ACCEPTABLE
-            return
-        }
-
-
-        if(!userInstance.invitationSent && userInstance.enabled && userInstance.username != "admin"){
-            userInstance.uuid = randomUUID() as String
-
-            log.debug("invitation email sent to $userInstance.username")
-
-            try{
-                sendMail {
-                    to userInstance.username
-                    subject "You have been invited!"
-                    body( view:"/mail/userInvite", model: [user: userInstance])
-                }
-            }catch (Exception e){}
-
-
-            userInstance.invitationSent = true
-        }
-
-
-        userInstance.save flush:true
-
-        UserRole.removeAll(userInstance)
-
-        data.authorities?.each{ roleJson ->
-          Role role = Role.get(roleJson.id)
-          UserRole.create(userInstance, role)
-        }
-
-        respond userInstance, [status: CREATED]
+    userInstance.validate()
+    if (userInstance.hasErrors()) {
+      render status: NOT_ACCEPTABLE
+      return
     }
 
 
-    def current(){
-      respond springSecurityService.currentUser, [status: OK]
+    if (!userInstance.invitationSent && userInstance.enabled && userInstance.username != "admin") {
+      userInstance.uuid = randomUUID() as String
+
+      log.debug("invitation email sent to $userInstance.username")
+
+      try {
+        sendMail {
+          to userInstance.username
+          subject "You have been invited!"
+          body(view: "/mail/userInvite", model: [user: userInstance])
+        }
+      } catch (Exception e) {
+      }
+
+
+      userInstance.invitationSent = true
     }
 
-    @Transactional
-    def makeUserAdmin(User userInstance) {
 
-        User currentUser = springSecurityService.currentUser
+    userInstance.save flush: true
 
-        if (userInstance == null) {
-            render status: NOT_FOUND
-            return
-        }
+    UserRole.removeAll(userInstance)
 
-        Role adminRole = Role.findByAuthority("ROLE_ADMIN")
-
-        if(!currentUser.authorities?.contains(adminRole)){
-            render status: UNAUTHORIZED
-            return
-        }
-
-        UserRole.create(userInstance, adminRole, true)
-
-        respond userInstance, [status: OK]
+    data.authorities?.each { roleJson ->
+      Role role = Role.get(roleJson.id)
+      UserRole.create(userInstance, role)
     }
 
-  def availableRoles(){
+    respond userInstance, [status: CREATED]
+  }
+
+
+  def current() {
+    respond springSecurityService.currentUser, [status: OK]
+  }
+
+  @Transactional
+  def makeUserAdmin(User userInstance) {
+
+    User currentUser = springSecurityService.currentUser
+
+    if (userInstance == null) {
+      render status: NOT_FOUND
+      return
+    }
+
+    Role adminRole = Role.findByAuthority("ROLE_ADMIN")
+
+    if (!currentUser.authorities?.contains(adminRole)) {
+      render status: UNAUTHORIZED
+      return
+    }
+
+    UserRole.create(userInstance, adminRole, true)
+
+    respond userInstance, [status: OK]
+  }
+
+  def availableRoles() {
     respond Role.list()
   }
 
-  def loginTarget(){
+  def loginTarget() {
     redirect(uri: '/')
   }
 
