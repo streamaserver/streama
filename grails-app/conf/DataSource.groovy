e4918fb948458921c441d7a781a7812d6709d962
whatevercode <florian.gr@gmx.net>
2015-08-03 21:17:39 +0200

    added dataSource for docker with environment variables MYSQL_HOST,MYSQL_PORT,MYSQL_DB,MYSQL_USER,MYSQL_PASSWORD

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -66,5 +66,48 @@ environments {
                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
         }
+        docker {
+          dataSource {
+            dbCreate = "update"
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+
+            //DEV
+            String host = System.getenv('MYSQL_HOST')
+            if(!host) host = "mysql"
+            String port = System.getenv('MYSQL_PORT')
+            if(!port) port = "3306"
+            String db = System.getenv('MYSQL_DB')
+            if(!db) db = "streama"
+            String user = System.getenv('MYSQL_USER')
+            if(!user) user = "streama"
+            String pass = System.getenv('MYSQL_PASSWORD')
+            if(!pass) pass = "streama"
+            url = "jdbc:mysql://$host:$port/$db"
+            username = "$user"
+            password = "$pass"
+
+            properties {
+              // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+              jmxEnabled = true
+              initialSize = 5
+              maxActive = 50
+              minIdle = 5
+              maxIdle = 25
+              maxWait = 10000
+              maxAge = 10 * 60000
+              timeBetweenEvictionRunsMillis = 5000
+              minEvictableIdleTimeMillis = 60000
+              validationQuery = "SELECT 1"
+              validationQueryTimeout = 3
+              validationInterval = 15000
+              testOnBorrow = true
+              testWhileIdle = true
+              testOnReturn = false
+              jdbcInterceptors = "ConnectionState"
+              defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
+          }
+        }
     }
 }
