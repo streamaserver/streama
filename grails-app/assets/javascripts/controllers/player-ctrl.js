79a16a093c77ed6b80aaa4e98e4828ecdc172847
dularion <antonia.hulha@systaro.de>
2015-08-10 18:39:23 +0200

    refactored player into player-ctrl and player-service. This works together with new directive

--- grails-app/assets/javascripts/controllers/player-ctrl.js
+++ grails-app/assets/javascripts/controllers/player-ctrl.js
@@ -1,285 +1,30 @@
 'use strict';
 
 streamaApp.controller('playerCtrl', [
-	'$scope', 'apiService', '$stateParams', '$timeout', '$rootScope', '$state', '$interval', '$sce', 'socketService', 'localStorageService',
-	function ($scope, apiService, $stateParams, $timeout, $rootScope, $state, $interval, $sce, socketService, localStorageService) {
-		$scope.loading = true;
-		var video = $('#video')[0];
-		var controlDisplayTimeout;
-		var overlayTimeout;
-		var timeCheckInterval;
-		var viewingStatusSaveInterval;
-		$scope.initialPlay = false;
-
-		$scope.volumeLevel = localStorageService.get('volumeLevel') || 5;
+	'$scope', 'apiService', '$stateParams', 'playerService', '$rootScope',
+	function ($scope, apiService, $stateParams, playerService, $rootScope) {
 
 		apiService.video.get($stateParams.videoId).success(function (data) {
 			$scope.video = data;
 
-			if(data.show){
-				apiService.tvShow.episodesForTvShow(data.show.id).success(function (episodes) {
-					$scope.groupedEpisodes = _.groupBy(episodes, 'season_number');
-					$scope.selectedEpisodes = $scope.groupedEpisodes[$scope.video.season_number];
-				});
+			var missingFileError = playerService.handleMissingFileError($scope.video);
+			var wrongBasePathError = playerService.handleWrongBasepathError($scope.video);
+
+			if(!missingFileError && !wrongBasePathError){
+				$scope.videoOptions = playerService.setVideoOptions($scope.video);
 			}
 
-      video.oncanplay = function() {
-        if($scope.initialPlay){
-          return;
-        }
-        $scope.initialPlay = true;
-        $scope.loading = false;
-        $scope.play();
-        $scope.videoDuration = video.duration;
+			playerService.registerSocketListener();
 
-        if($stateParams.currentTime){
-          $scope.currentTime = $stateParams.currentTime;
-        }
-        else if(data.viewedStatus){
-          $scope.currentTime = data.viewedStatus.currentPlayTime;
-        }else{
-          $scope.currentTime = 0;
-        }
-        video.currentTime = $scope.currentTime;
-      };
-
-      video.onwaiting = function() {
-        $scope.loading = true;
-      };
-
-      video.onplaying = function() {
-        $scope.loading = false;
-      };
-
-      $timeout(function () {
-        if(!video.duration){
-          alertify.alert('There seems to be a problem adding the video-file to the player. This is most likely due to a codec-problem. ' +
-            'Try converting it to a compatible HTML5 codec, remove the currently attached file and re-add it. If the codecs are fine, check the error log of the server.', function () {
-            if($rootScope.currentUser.authorities.length){
-              if($scope.video.show){
-                $state.go('admin.show', {showId: $scope.video.show.id});
-              }else{
-                $state.go('admin.movie', {movieId: $scope.video.id});
-              }
-            }else{
-              $state.go('dash');
-            }
-
-          });
-        }
-      }, 5000);
 		});
 
-		$scope.toggleSelectEpisodes = function (episodes) {
-			$scope.selectedEpisodes = episodes;
-		};
-
-		$scope.createNewPlayerSession = function () {
-			alertify.confirm('By creating a new session you will be redirected back to this player, but this time you will ' +
-			'have a unique session ID in the url. Share this with your friends to have a syncronized watching experience with them!', function (confirmed) {
-				if(confirmed){
-					$stateParams.sessionId = socketService.getUUID();
-					$state.go($state.current, $stateParams, {reload: true});
-				}
-			});
-		};
-
-
-		if($stateParams.sessionId){
-			socketService.registerPlayerSessonListener($stateParams.sessionId);
-
-			$scope.$on('playerSession', function (e, data) {
-				console.log('%c playerSessionSocket', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;', data);
-				if(data.browserSocketUUID != socketService.browserSocketUUID){
-					if(data.playerAction == 'play'){
-						$scope.play(true);
-					}else if(data.playerAction == 'pause'){
-						$scope.pause(true);
-						if(video.currentTime+1.5 > data.currentPlayerTime || video.currentTime-1.5 < data.currentPlayerTime){
-							video.currentTime = data.currentPlayerTime;
-						}
-					}
-				}
-
-			});
-		}
-
-		Mousetrap.bind('space', function() {
-			if($scope.playing){
-				$scope.pause();
-			}else{
-				$scope.play();
-			}
-		});
-
-		$scope.trustSrc = function(src) {
-			return $sce.trustAsResourceUrl(src);
-		};
-
-		//$scope.controlsVisible = true;
-		$scope.showControls = function () {
-			$timeout.cancel(controlDisplayTimeout);
-			$timeout.cancel(overlayTimeout);
-			$scope.controlsVisible = true;
-			$scope.overlayVisible = false;
-
-
-			controlDisplayTimeout = $timeout(function(){
-				$scope.controlsVisible = false;
-
-				if(!$scope.playing)
-					overlayTimeout = $timeout(function () {
-						if(!$scope.playing){
-							$scope.overlayVisible = true;
-						}
-					}, 5000);
-			}, 3000);
-		};
-
-		$scope.playerVolumeToggle = function () {
-			if($scope.volumeLevel == 0){
-				$scope.volumeLevel = 6;
-			}else{
-				$scope.volumeLevel = 0;
-			}
-		};
-
-		$scope.scrubberOptions = {
-			orientation: 'horizontal',
-			min: 0,
-			max: 255,
-			range: 'min',
-			change: function (e, slider) {
-				//console.log('%c slider.value', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;', slider.value);
-				//video.currentTime = slider.value;
-			},
-			stop: function (e, slider) {
-				video.currentTime = slider.value;
-
-				var params = {videoId: $scope.video.id, currentTime: slider.value, runtime: $scope.videoDuration};
-				apiService.viewingStatus.save(params);
-			}
-		};
-
-		$scope.volumeScrubberOptions = {
-			orientation: 'vertical',
-			range: 'min',
-			change: function (e, slider) {
-				var volume = slider.value / 10;
-				video.volume = volume;
-        localStorageService.set('volumeLevel', $scope.volumeLevel);
-			},
-			slide: function (e, slider) {
-				var volume = slider.value / 10;
-				video.volume = volume;
-        localStorageService.set('volumeLevel', $scope.volumeLevel);
-			}
-		};
-
-
-
-		$scope.backToDash = function () {
-			destroyPlayer();
-			$state.go('dash');
-		};
-
-		$scope.nextEpisode = function () {
-			destroyPlayer();
-			$state.go('player', {videoId: $scope.video.nextEpisode.id});
-		};
-
-		$scope.play = function (excludeSocket) {
-			video.play();
-			$scope.playing = true;
-
-			timeCheckInterval = $interval(function() {
-				$scope.currentTime = video.currentTime;
-			}, 1000);
-
-			viewingStatusSaveInterval = $interval(function() {
-				var params = {videoId: $scope.video.id, currentTime: $scope.currentTime, runtime: $scope.videoDuration};
-
-        if(params.runtime && params.videoId){
-          apiService.viewingStatus.save(params);
-        }
-			}, 5000);
-
-
-			if($stateParams.sessionId && !excludeSocket){
-				apiService.websocket.triggerPlayerAction({socketSessionId: $stateParams.sessionId, playerAction: 'play', currentPlayerTime: video.currentTime});
-			}
-		};
-
-
-		$scope.pause = function (excludeSocket) {
-			video.pause();
-			$scope.playing = false;
-			$interval.cancel(viewingStatusSaveInterval);
-			$interval.cancel(timeCheckInterval);
-
-
-			if($stateParams.sessionId && !excludeSocket){
-				apiService.websocket.triggerPlayerAction({socketSessionId: $stateParams.sessionId, playerAction: 'pause', currentPlayerTime: video.currentTime});
-			}
-		};
-
-		$scope.getStyleProgress = function (attribute) {
-			var percentage = video.currentTime /  video.duration * 100;
-			var returnObj = {};
-			returnObj[attribute] = percentage+'%';
-			return returnObj;
-		};
-
-		$scope.fullScreen = function () {
-			$scope.isFullScreen = !$scope.isFullScreen;
-
-			if($scope.isFullScreen){
-				var docElm = document.documentElement;
-				if (docElm.requestFullscreen) {
-					docElm.requestFullscreen();
-				}
-				else if (docElm.mozRequestFullScreen) {
-					docElm.mozRequestFullScreen();
-				}
-				else if (docElm.webkitRequestFullScreen) {
-					docElm.webkitRequestFullScreen();
-				}
-				else if (docElm.msRequestFullscreen) {
-					docElm.msRequestFullscreen();
-				}
-			}
-
-			else{
-				if (document.exitFullscreen) {
-					document.exitFullscreen();
-				}
-				else if (document.mozCancelFullScreen) {
-					document.mozCancelFullScreen();
-				}
-				else if (document.webkitCancelFullScreen) {
-					document.webkitCancelFullScreen();
-				}
-				else if (document.msExitFullscreen) {
-					document.msExitFullscreen();
-				}
-			}
-		};
-
-
-		var destroyPlayer = function () {
-			video.pause();
-			video.src = '';
-			$(video).children('source').prop('src', '');
-			$(video).remove().length = 0;
-			$interval.cancel(viewingStatusSaveInterval);
-			$interval.cancel(timeCheckInterval);
-		};
-
-		$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+		$rootScope.$on('$stateChangeStart', function(e, toState){
 			if(toState.name != 'player'){
-				destroyPlayer();
+				playerService.destroyPlayer();
 			}
 		});
 
-
-}]);
+		$scope.$on('playerSession', function (e, data) {
+			playerService.handleSocketEvent(data);
+		});
+	}]);
---
+++ grails-app/assets/javascripts/services/player-service.js
@@ -0,0 +1,249 @@
+'use strict';
+
+streamaApp.factory('playerService', [
+  '$stateParams', '$sce', '$state', '$rootScope', 'socketService', 'apiService', '$interval',
+  function ($stateParams, $sce, $state, $rootScope, socketService, apiService, $interval) {
+
+    var videoData = null;
+
+    var videoOptions = {
+      customStartingTime: 0,
+      rememberVolumeSetting: true,
+      videoMetaTitle: '',
+      videoMetaSubtitle: '',
+      videoMetaDescription: '',
+      videoSrc: '',
+      videoType: '',
+      videoTrack: '',
+      videoOverlayEnabled: true,
+      showEpisodeBrowser: false,
+      showNextButton: false,
+      showSocketSession: true,
+      episodeList: [],
+      selectedEpisodes: [],
+      currentEpisode: {},
+      onSocketSessionCreate: angular.noop,
+      onTimeChange: angular.noop,
+      onError: angular.noop,
+      onPlay: angular.noop,
+      onPause: angular.noop,
+      onClose: angular.noop,
+      onNext: angular.noop
+    };
+
+    return {
+
+      setVideoOptions: function (video) {
+        videoData = video;
+        videoOptions.videoSrc = $sce.trustAsResourceUrl(video.files[0].src);
+        videoOptions.videoType = video.files[0].contentType;
+
+        if(video.subtitles && video.subtitles.length){
+          videoOptions.videoTrack = $sce.trustAsResourceUrl(video.subtitles[0].src);
+        }
+
+        videoOptions.videoMetaTitle = (video.show ? video.show.name : video.title);
+        videoOptions.videoMetaSubtitle = (video.show ? video.episodeString + ' - ' + video.name : video.release_date.substring(0, 4));
+        videoOptions.videoMetaDescription = video.overview;
+
+        if(videoData.nextEpisode){
+          videoOptions.showNextButton = true;
+        }
+
+        if(videoData.show){
+          videoOptions.showEpisodeBrowser = true;
+
+          apiService.tvShow.episodesForTvShow(videoData.show.id).success(function (episodes) {
+            videoOptions.episodeList = _.groupBy(episodes, 'season_number');
+            videoOptions.selectedEpisodes = videoOptions.episodeList[videoData.season_number];
+            videoOptions.currentEpisode = {
+              episode: videoData.episode_number,
+              season: videoData.season_number
+            };
+          });
+        }
+
+        if($stateParams.currentTime){
+          videoOptions.customStartingTime = $stateParams.currentTime;
+        }
+        else if(video.viewedStatus){
+          videoOptions.customStartingTime = video.viewedStatus.currentPlayTime;
+        }else{
+          videoOptions.customStartingTime = 0;
+        }
+
+        videoOptions.onPlay = this.onVideoPlay.bind(videoOptions);
+        videoOptions.onPause = this.onVideoPause.bind(videoOptions);
+        videoOptions.onError = this.onVideoError.bind(videoOptions);
+        videoOptions.onTimeChange = this.onVideoTimeChange.bind(videoOptions);
+        videoOptions.onClose = this.onVideoClose.bind(videoOptions);
+        videoOptions.onNext = this.onNext.bind(videoOptions);
+        videoOptions.onSocketSessionCreate = this.onSocketSessionCreate.bind(videoOptions);
+
+        return videoOptions;
+      },
+
+      viewingStatusSaveInterval: null,
+
+      onVideoPlay: function (videoElement, socketData) {
+        var that = this;
+        console.log('%c onVideoPlay', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;');
+
+        that.viewingStatusSaveInterval = $interval(function() {
+          var params = {videoId: videoData.id, currentTime: videoElement.currentTime, runtime: videoElement.duration};
+
+          if(params.runtime && params.videoId){
+            apiService.viewingStatus.save(params);
+          }
+        }, 5000);
+
+
+        if($stateParams.sessionId && !socketData){
+          console.log('%c send socket event PLAY', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;');
+          apiService.websocket.triggerPlayerAction({socketSessionId: $stateParams.sessionId, playerAction: 'play', currentPlayerTime: videoElement.currentTime});
+        }
+      },
+
+      onVideoPause: function (videoElement, socketData) {
+        console.log('%c onVideoPause', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;', socketData);
+        var that = this;
+        $interval.cancel(that.viewingStatusSaveInterval);
+
+        if($stateParams.sessionId && socketData){
+          if(videoElement.currentTime+1.5 > socketData.currentPlayerTime || videoElement.currentTime-1.5 < socketData.currentPlayerTime){
+            videoElement.currentTime = socketData.currentPlayerTime;
+          }
+        }
+
+
+        if($stateParams.sessionId && !socketData){
+          console.log('%c send socket event PAUSE', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;');
+          apiService.websocket.triggerPlayerAction({socketSessionId: $stateParams.sessionId, playerAction: 'pause', currentPlayerTime: videoElement.currentTime});
+        }
+      },
+
+      onVideoClose: function () {
+        console.log('%c onVideoClose', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;');
+        var that = this;
+        $state.go('dash', {});
+      },
+
+      onVideoError: function () {
+        var that = this;
+        console.log('%c onVideoError', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;');
+
+        if($state.current.name == 'player'){
+          alertify.alert('There seems to be a problem adding the video-file to the player. This is most likely due to a codec-problem. ' +
+            'Try converting it to a compatible HTML5 codec, remove the currently attached file and re-add it. If the codecs are fine, check the error log of the server.', function () {
+            if($rootScope.currentUser.authorities.length){
+              if(video.show){
+                $state.go('admin.show', {showId: videoData.show.id});
+              }else{
+                $state.go('admin.movie', {movieId: videoData.id});
+              }
+            }else{
+              $state.go('dash', {});
+            }
+
+          });
+        }
+      },
+
+      onVideoTimeChange: function (slider, duration) {
+        var params = {videoId: videoData.id, currentTime: slider.value, runtime: duration};
+        apiService.viewingStatus.save(params);
+
+
+        if($stateParams.sessionId){
+          apiService.websocket.triggerPlayerAction({socketSessionId: $stateParams.sessionId, playerAction: 'timeChange', currentPlayerTime: slider.value});
+        }
+      },
+
+      onSocketSessionCreate: function () {
+        alertify.confirm('By creating a new session you will be redirected back to this player, but this time you will ' +
+          'have a unique session ID in the url. Share this with your friends to have a syncronized watching experience with them!', function (confirmed) {
+          if(confirmed){
+            $stateParams.sessionId = socketService.getUUID();
+            $state.go($state.current, $stateParams, {reload: true});
+          }
+        });
+      },
+
+      handleMissingFileError: function (video) {
+        var hasError = false;
+
+        if(!video.files || !video.files.length){
+          hasError = true;
+          alertify.alert('There is a problem with this content. It seems you removed the associated video fime from it.', function () {
+            if($rootScope.currentUser.authorities.length){
+              if(video.show){
+                $state.go('admin.show', {showId: video.show.id});
+              }else{
+                $state.go('admin.movie', {movieId: video.id});
+              }
+            }else{
+              $state.go('dash', {});
+            }
+
+          });
+        }
+
+        return hasError;
+      },
+
+      handleWrongBasepathError: function (video) {
+        var hasError = false;
+        var videoSource = video.files[0].src;
+        var basePath = location.origin + $('base').attr('href');
+
+        if(videoSource.indexOf(basePath) == -1){
+          hasError = true;
+          alertify.alert('You video get\'s included using the wrong Base Path, but you are browsing the page via "'+basePath+'". ' +
+            'Make sure you set the correct Base Path in the settings and that you are using it to browse the application.', function () {
+            if(_.find($rootScope.currentUser.authorities, {authority: "ROLE_ADMIN"})){
+              $state.go('admin.settings');
+            }else{
+              $state.go('dash', {});
+            }
+
+          });
+        }
+        return hasError;
+      },
+
+      registerSocketListener: function () {
+        if($stateParams.sessionId){
+          socketService.registerPlayerSessonListener($stateParams.sessionId);
+        }
+      },
+
+      destroyPlayer: function () {
+        console.log('%c $stateChangeSuccess', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;');
+        var that = this;
+        $interval.cancel(that.viewingStatusSaveInterval);
+        socketService.unsubscribe();
+      },
+
+      handleSocketEvent: function (data) {
+        if(data.browserSocketUUID != socketService.browserSocketUUID){
+          console.log('%c handleSocketEvent', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;');
+          switch (data.playerAction){
+            case 'play':
+              $rootScope.$broadcast('triggerVideoPlay', data);
+              break;
+            case 'pause':
+              $rootScope.$broadcast('triggerVideoPause', data);
+              break;
+            case 'timeChange':
+              $rootScope.$broadcast('triggerVideoTimeChange', data);
+              break;
+          }
+        }
+      },
+
+
+      onNext: function () {
+        $state.go('player', {videoId: videoData.nextEpisode.id});
+      }
+    };
+}]);
