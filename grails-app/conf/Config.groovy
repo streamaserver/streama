f95b3df841dd572654e4cbafd65bb0a907550781
dularion <antonia.hulha@systaro.de>
2015-08-22 16:19:08 +0200

    using external config file

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,6 +11,8 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+grails.config.locations = ['file:/streama-data/config.properties']
+
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
