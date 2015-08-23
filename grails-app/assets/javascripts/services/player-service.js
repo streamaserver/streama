d41560d59aa550e1de3df9bc6a62b6054e22dd52
dularion <antonia.hulha@systaro.de>
2015-08-23 16:23:08 +0200

    fixed redirect

--- grails-app/assets/javascripts/services/player-service.js
+++ grails-app/assets/javascripts/services/player-service.js
@@ -136,7 +136,7 @@ streamaApp.factory('playerService', [
           alertify.alert('There seems to be a problem adding the video-file to the player. This is most likely due to a codec-problem. ' +
             'Try converting it to a compatible HTML5 codec, remove the currently attached file and re-add it. If the codecs are fine, check the error log of the server.', function () {
             if($rootScope.currentUser.authorities.length){
-              if(video.show){
+              if(videoData.show){
                 $state.go('admin.show', {showId: videoData.show.id});
               }else{
                 $state.go('admin.movie', {movieId: videoData.id});
