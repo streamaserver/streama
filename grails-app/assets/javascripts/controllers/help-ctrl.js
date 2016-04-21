a377e68501bba480502d0e3340160e356f0e6c86
Janne <janneniemela90@gmail.com>
2016-04-21 22:31:29 +0300

    Added more information to the Settings page's API-key text
    -Also removed the API-key section from the Help page, because it became
    redundant.

--- grails-app/assets/javascripts/controllers/help-ctrl.js
+++ grails-app/assets/javascripts/controllers/help-ctrl.js
@@ -20,13 +20,6 @@ streamaApp.controller('helpCtrl', ['$anchorScroll', '$location', '$scope',
     $scope.questions = [
       {
         id: 1,
-        name: "What's an API-key and why do I need one?",
-        answer: "API stands for Application Programming Interface. TheMovieDB's movie/TV show information database is accessible only by registering to that website and" +
-        " requesting an API-key. After you have sent a request form, you can see your API-key by going to the API section in your profile's settings in TheMovieDB." +
-        " Copy your key into Streama's settings, and you can load movies/TV shows' information automatically."
-      },
-      {
-        id: 2,
         name: 'How can I upload a video?',
         answer: "You can upload videos by going to Manage Content menu. Choose if you want to upload a Movie, TV show or Other video. Click the relevant sub-menu option" +
         " on the vertical navigation bar on the left side of the screen. You can upload a video by clicking the Create new movie/TV show/other video button or by typing" +
@@ -34,26 +27,26 @@ streamaApp.controller('helpCtrl', ['$anchorScroll', '$location', '$scope',
         " information either manually or loading its information from TheMovieDB. After that, you can upload the relevant video and subtitle files by clicking Manage Files button."
       },
       {
-        id: 3,
+        id: 2,
         name: 'How can I delete a video?',
         answer: "You can delete a video by going to the video's information page and clicking Manage Files and selecting the red trash can icon. Clicking Edit movie and selecting" +
         " Delete movie is another way to do it. You can also use the File Manager which is in the Manage Content menu. You can see all the files you have uploaded there. Click" +
         " the red trash can icon to delete a file."
       },
       {
-        id: 4,
+        id: 3,
         name: 'Which video formats are supported?',
         answer: "Streama supports currently only the video file formats supported by HTML5 player. You can test if your video file is HTML5 player compatible by dragging and dropping" +
         " your file to an empty tab on your browser."
       },
       {
-        id: 5,
+        id: 4,
         name: 'How can I add subtitles to videos?',
         answer: "You can add subtitles to videos by clicking Manage Files button which is in the video's information page. You can drag and drop subtitle files there." +
         " Please note that the subtitles must be .vtt format. You can use the website which link is in the Manage Files popup to convert .srt type subtitle files to .vtt."
       },
       {
-        id: 6,
+        id: 5,
         name: "How can I invite friends to watch my hosted videos?",
         answer: "You can share your videos with your friends by inviting them to use your hosted Streama. Go to the Users menu and click Invite user button. Fill in the invite form and" +
         " select the invitee's role(s). Users with the role Admin can edit Users & Settings. Users with the role Content Manager can edit content. Your friend will be notified about" +
@@ -61,29 +54,29 @@ streamaApp.controller('helpCtrl', ['$anchorScroll', '$location', '$scope',
 
       },
       {
-        id: 7,
+        id: 6,
         name: "What's the base URL and how should I configure it?",
         answer: "The Base-URL is used for the videos and the link in the invitation-email."
       },
       {
-        id: 8,
+        id: 7,
         name: "What are notifications?",
         answer: "You can notify your invited friends by creating notification messages about uploaded videos."
       },
       {
-        id: 9,
+        id: 8,
         name: 'Does the video player have shortcut keys?',
         answer: "Yes. Pause/unpause: space. Manage volume: arrow keys up or down. Skip video forward/backward: arrow keys left or right. Long skip:" +
         " control + arrow keys left or right. Fullscreen on/off: alt + enter. Subtitles on/off: S, Mute: M, Return to previous" +
         " screen: delete or backspace."
       },
       {
-        id: 10,
+        id: 9,
         name: "How do user's favourite genres affect Streama?",
         answer: ""
       },
       {
-        id: 11,
+        id: 10,
         name: 'Useful links',
         answer: ""
       }
