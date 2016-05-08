c93f578d4a09a397fb36a531aef69a3419a0bd45
dularion <antonia.hulha@systaro.de>
2016-05-08 20:08:35 +0200

    split js and css dependencies into vendor and application, vendor only contains requires for bower libraries

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,23 +5,5 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require spring-websocket
-//= require jquery/dist/jquery.min.js
-//= require angular/angular.min.js
-//= require angular-ui-router/release/angular-ui-router.min.js
-//= require angular-sanitize/angular-sanitize.min.js
-//= require bootstrap/dist/js/bootstrap.min.js
-//= require angular-bootstrap/ui-bootstrap.min.js
-//= require angular-bootstrap/ui-bootstrap-tpls.min.js
-//= require alertify/alertify.min.js
-//= require lodash/dist/lodash.min.js
-//= require ng-file-upload/ng-file-upload.min.js
-//= require jquery-ui-1.11.4.custom/jquery-ui.min.js
-//= require angular-ui-slider/src/slider.js
-//= require mousetrap/mousetrap.min.js
-//= require Autolinker.js/dist/Autolinker.js
-//= require angular-local-storage/dist/angular-local-storage.min.js
-//= require angular-ui-select/dist/select.min.js
-//= require jquery-mousewheel/jquery.mousewheel.min.js
 //= require streama-app.js
 //= require_tree .
