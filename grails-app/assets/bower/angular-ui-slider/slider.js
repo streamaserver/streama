ec017569a0532d0c63475eff32eb3b2164bc4a33
dularion <antonia.hulha@systaro.de>
2016-05-08 20:07:01 +0200

    added new dependencies as only main files in /bower

--- grails-app/assets/bower/angular-ui-slider/slider.js
+++ grails-app/assets/bower/angular-ui-slider/slider.js
@@ -12,7 +12,8 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
                     return (decimals) ? parseFloat(n) : parseInt(n, 10);
                 }
 
-                var options = angular.extend(scope.$eval(attrs.uiSlider) || {}, uiSliderConfig);
+                var directiveOptions = angular.copy(scope.$eval(attrs.uiSlider));
+                var options = angular.extend(directiveOptions || {}, uiSliderConfig);
                 // Object holding range values
                 var prevRangeValues = {
                     min: null,
@@ -124,7 +125,7 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
                 ngModel.$render = function() {
                     init();
                     var method = options.range === true ? 'values' : 'value';
-                    
+
                     if (options.range !== true && isNaN(ngModel.$viewValue) && !(ngModel.$viewValue instanceof Array)) {
                         ngModel.$viewValue = 0;
                     }
@@ -134,7 +135,13 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
 
                     // Do some sanity check of range values
                     if (options.range === true) {
-                        
+                        // previously, the model was a string b/c it was in a text input, need to convert to a array.
+                        // make sure input exists, comma exists once, and it is a string.
+                        if (ngModel.$viewValue && angular.isString(ngModel.$viewValue) && (ngModel.$viewValue.match(/,/g) || []).length === 1) {
+                            // transform string model into array.
+                            var valueArr = ngModel.$viewValue.split(',');
+                            ngModel.$viewValue = [Number(valueArr[0]), Number(valueArr[1])];
+                        }
                         // Check outer bounds for min and max values
                         if (angular.isDefined(options.min) && options.min > ngModel.$viewValue[0]) {
                             ngModel.$viewValue[0] = options.min;
@@ -147,11 +154,11 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
                         if (ngModel.$viewValue[0] > ngModel.$viewValue[1]) {
                             // Min value should be less to equal to max value
                             if (prevRangeValues.min >= ngModel.$viewValue[1]) {
-                                ngModel.$viewValue[0] = prevRangeValues.min;
+                                ngModel.$viewValue[1] = prevRangeValues.min;
                             }
                             // Max value should be less to equal to min value
                             if (prevRangeValues.max <= ngModel.$viewValue[0]) {
-                                ngModel.$viewValue[1] = prevRangeValues.max;
+                                ngModel.$viewValue[0] = prevRangeValues.max;
                             }
                         }
 
@@ -183,14 +190,14 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
                 // Add tick marks if 'tick' and 'step' attributes have been setted on element.
                 // Support horizontal slider bar so far. 'tick' and 'step' attributes are required.
                 var options = angular.extend({}, scope.$eval(attrs.uiSlider));
-                var properties = ['max', 'step', 'tick'];
+                var properties = ['min', 'max', 'step', 'tick'];
                 angular.forEach(properties, function(property) {
                     if (angular.isDefined(attrs[property])) {
                         options[property] = attrs[property];
                     }
                 });
                 if (angular.isDefined(options['tick']) && angular.isDefined(options['step'])) {
-                    var total = parseInt(parseInt(options['max'])/parseInt(options['step']));
+                    var total = parseInt( (parseInt(options['max']) - parseInt(options['min'])) /parseInt(options['step']));
                     for (var i = total; i >= 0; i--) {
                         var left = ((i / total) * 100) + '%';
                         $("<div/>").addClass("ui-slider-tick").appendTo(element).css({left: left});
