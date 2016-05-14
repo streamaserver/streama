fc777bbc3d9e4a601e0114caa5a56a9bf12825bc
dularion <antonia.hulha@systaro.de>
2016-05-14 21:54:47 +0200

    refactored routs

--- grails-app/assets/javascripts/streama-app.js
+++ grails-app/assets/javascripts/streama-app.js
@@ -7,6 +7,7 @@ var streamaApp = angular.module('streamaApp', [
 	'ui.bootstrap',
 	'ngFileUpload',
 	'ui.slider',
+	'pascalprecht.translate',
 	'LocalStorageModule',
 	'ui.select',
 	'ngSanitize',
@@ -15,195 +16,17 @@ var streamaApp = angular.module('streamaApp', [
 
 
 
-streamaApp.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
+streamaApp.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', '$translateProvider',
+	function ($stateProvider, $urlRouterProvider, $httpProvider, $translateProvider) {
 
-	$stateProvider
-		.state('dash', {
-			url: '/dash?genreId?mediaModal?mediaType',
-			templateUrl: 'dash.htm',
-			controller: 'dashCtrl',
-			reloadOnSearch: false,
-      resolve: {
-        currentUser: ['apiService', '$rootScope', function (apiService, $rootScope) {
-          return apiService.currentUser().success(function (data) {
-            if (data) {
-              $rootScope.currentUser = data;
-              return data;
-            }
-          });
-        }]
-      }
-		})
-		.state('settings.profile', {
-			url: '/profile',
-			templateUrl: 'profile.htm',
-			controller: 'settingsProfileCtrl',
-      resolve: {
-        currentUser: ['apiService', '$rootScope', function (apiService, $rootScope) {
-          return apiService.currentUser().success(function (data) {
-            if (data) {
-              $rootScope.currentUser = data;
-              return data;
-            }
-          });
-        }]
-      }
-		})
-		.state('player', {
-			url: '/player/:videoId?currentTime?sessionId',
-			templateUrl: 'player.htm',
-			controller: 'playerCtrl',
-      resolve: {
-        currentUser: ['apiService', '$rootScope', function (apiService, $rootScope) {
-          return apiService.currentUser().success(function (data) {
-            if (data) {
-              $rootScope.currentUser = data;
-              return data;
-            }
-          });
-        }]
-      }
-		})
-		.state('admin', {
-			url: '/admin',
-			templateUrl: 'admin.htm',
-			controller: 'adminCtrl',
-      resolve: {
-        currentUser: ['apiService', '$rootScope', '$state', function (apiService, $rootScope, $state) {
-          return apiService.currentUser().success(function (data) {
-            if (data && data.authorities.length) {
-              $rootScope.currentUser = data;
-              return data;
-            } else {
-              $state.go('dash');
-            }
-          });
-        }]
-      }
-		})
-		.state('admin.fileManager', {
-			url: '/fileManager',
-			templateUrl: 'admin-fileManager.htm',
-			controller: 'adminFileManagerCtrl'
-		})
-		.state('admin.movies', {
-			url: '/movies',
-			templateUrl: 'admin-movies.htm',
-			controller: 'adminMoviesCtrl'
-		})
-		.state('admin.movie', {
-			url: '/movie/:movieId',
-			templateUrl: 'admin-movie.htm',
-			controller: 'adminMovieCtrl'
-		})
-		.state('admin.videos', {
-			url: '/videos',
-			templateUrl: 'admin-videos.htm',
-			controller: 'adminVideosCtrl'
-		})
-		.state('admin.video', {
-			url: '/video/:videoId',
-			templateUrl: 'admin-video.htm',
-			controller: 'adminVideoCtrl'
-		})
-		.state('settings.users', {
-			url: '/users',
-			templateUrl: 'settings-users.htm',
-			controller: 'settingsUsersCtrl',
-      resolve: {
-        currentUser: ['apiService', '$rootScope', '$state', function (apiService, $rootScope, $state) {
-          return apiService.currentUser().success(function (data) {
-            if (data && data.isAdmin) {
-              $rootScope.currentUser = data;
-              return data;
-            } else {
-              $state.go('dash');
-            }
-          });
-        }]
-      }
-		})
-		.state('settings.settings', {
-			url: '/settings',
-			templateUrl: 'settings-settings.htm',
-			controller: 'settingsSettingsCtrl',
-      resolve: {
-        currentUser: ['apiService', '$rootScope', '$state', function (apiService, $rootScope, $state) {
-          return apiService.currentUser().success(function (data) {
-            if (data.isAdmin) {
-              $rootScope.currentUser = data;
-              return data;
-            } else {
-              $state.go('dash');
-            }
-          });
-        }]
-      }
-		})
-		.state('admin.notifications', {
-			url: '/notifications',
-			templateUrl: 'admin-notifications.htm',
-			controller: 'adminNotificationsCtrl',
-      resolve: {
-        currentUser: ['apiService', '$rootScope', '$state', function (apiService, $rootScope, $state) {
-          return apiService.currentUser().success(function (data) {
-            if (data.isAdmin) {
-              $rootScope.currentUser = data;
-              return data;
-            } else {
-              $state.go('dash');
-            }
-          });
-        }]
-      }
-		})
-
-		.state('admin.newReleases', {
-			url: '/newReleases',
-			templateUrl: 'admin-new-releases.htm',
-			controller: 'adminNewReleasesCtrl'
-		})
-		.state('admin.shows', {
-			url: '/shows',
-			templateUrl: 'admin-shows.htm',
-			controller: 'adminShowsCtrl'
-		})
-		.state('admin.show', {
-			url: '/show/:showId',
-			templateUrl: 'admin-show.htm',
-			controller: 'adminShowCtrl'
-		})
-    .state('help', {
-      url: '/help',
-      templateUrl: 'help.htm',
-      controller: 'helpCtrl'
-    })
-    .state('settings', {
-      url: '/settings',
-      templateUrl: 'settings.htm',
-      controller: 'settingsCtrl',
-      resolve: {
-        currentUser: ['apiService', '$rootScope', '$state', function (apiService, $rootScope, $state) {
-          return apiService.currentUser().success(function (data) {
-            if (data && data.authorities.length) {
-              $rootScope.currentUser = data;
-              return data;
-            } else {
-              $state.go('dash');
-            }
-          });
-        }]
-      }
-    });
-
-
+	$translateProvider.preferredLanguage('en');
 	$urlRouterProvider.otherwise('/dash');
-
 
 	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 	$httpProvider.interceptors.push('httpInterceptor');
 
 }])
+
 
 	.factory('httpInterceptor', ['$rootScope', '$q', function ($rootScope, $q) {
 		return {
