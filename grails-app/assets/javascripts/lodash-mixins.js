84b2bc865560c7771916004e15533ef7f23c4ed4
dularion <antonia.hulha@systaro.de>
2016-05-15 00:37:04 +0200

    restyled code in lodash-mixins (even though they dont work yet)

--- grails-app/assets/javascripts/lodash-mixins.js
+++ grails-app/assets/javascripts/lodash-mixins.js
@@ -1,9 +1,15 @@
-_.mixin({
-	'pushOrReplace' : function(list, object) {
+(function () {
+	_.mixin({
+		pushOrReplace: pushOrReplace
+	});
+
+
+	function pushOrReplace(list, object) {
 		if(_.find(list, {id: object.id})){
 			list[_.indexOf(list, {id: object.id})] = object;
 		}else{
 			list.push(object);
 		}
 	}
-});
+
+})();
