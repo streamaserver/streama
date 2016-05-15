d7b264a54ca3709992a47cc579f55389e2ccee4e
dularion <antonia.hulha@systaro.de>
2016-05-15 17:38:09 +0200

    translated FAQ

--- grails-app/assets/javascripts/translations/EN_us.js
+++ grails-app/assets/javascripts/translations/EN_us.js
@@ -75,6 +75,61 @@ angular.module('streama.translations').config(function ($translateProvider) {
 			OLDEST_RELEASED: 'Oldest Release',
 			NEWEST_AIRED: 'Most Recently Aired',
 			OLDEST_AIRED: 'Oldest Air-Date',
+		},
+
+		FAQ: {
+			UPLOAD_VIDEO: {
+				TITLE: 'How can I upload a video?',
+				TEXT: "You can upload videos by going to Manage Content menu. Choose if you want to upload a Movie, TV show or Other video. Click the relevant sub-menu option" +
+				" on the vertical navigation bar on the left side of the screen. You can upload a video by clicking the Create New Movie/TV Show/Other Video button or by typing" +
+				" the name of the video you want to upload to the search bar and selecting the relevant movie from search results. After that, you can choose to fill in the video's" +
+				" information either manually or loading its information from TheMovieDB. After that, you can upload the video and subtitle files by clicking Manage Files button."
+			},
+			DELETE_VIDEO: {
+				TITLE: 'How can I delete a video?',
+				TEXT: "You can delete a video by going to the video's information page and clicking Manage Files and selecting the red trash can icon. Clicking Edit Movie and selecting" +
+				" Delete Movie is another way to do it. You can also use the File Manager which is in the Manage Content menu. You can see all the files you have uploaded there. Click" +
+				" the red trash can icon to delete a file."
+			},
+			VIDEO_FORMATS: {
+				TITLE: 'Which video formats are supported?',
+				TEXT: "Streama supports currently only the video file formats supported by HTML5 player. You can test if your video file is HTML5 player compatible by dragging and dropping" +
+				" your file to an empty tab on your browser."
+			},
+			SUBTITLES: {
+				TITLE: 'How can I add subtitles to videos?',
+				TEXT: "You can add subtitles to videos by clicking Manage Files button which is in the video's information page. You can drag and drop subtitle files there." +
+				" Please note that the subtitles must be .vtt format. You can use the website which link is in the Manage Files popup to convert .srt type subtitle files to .vtt."
+			},
+			INVITE_USERS: {
+				TITLE: 'How can I invite friends to watch my hosted videos?',
+				TEXT:"You can share your videos with your friends by inviting them to use your hosted Streama. Go to the Users menu and click Invite User button. Fill in the invite form and" +
+				" select the invitee's role(s). Users with the role Admin can edit Users & Settings. Users with the role Content Manager can edit content. Your friend will be notified about" +
+				" the invitation via email. You can also share video sessions with your friends by clicking the video player's Share button and linking the session URL to them."
+			},
+			BASE_URL: {
+				TITLE: "What's the base URL and how should I configure it?",
+				TEXT: "The Base-URL is used for the videos and the link in the invitation-email."
+			},
+			NOTIFICATIONS: {
+				TITLE: "What are notifications?",
+				TEXT: "You can notify your invited friends about uploaded videos by sending them notification messages. You can send them by adding them your notification queue by clicking" +
+				" Add Notification button which is in your video's information page and going to the Notifications menu and clicking Send Queue button."
+			},
+			VIDEO_PLAYER_SHORTCUTS: {
+				TITLE: "Does the video player have shortcut keys?",
+				TEXT: "Yes. Pause/unpause: space. Manage volume: arrow keys up or down. Skip video forward/backward: arrow keys left or right. Long skip:" +
+				" control + arrow keys left or right. Fullscreen on/off: alt + enter. Subtitles on/off: S, Mute: M, Return to previous" +
+				" screen: delete or backspace."
+			},
+			FAVORITE_GENRES: {
+				TITLE: "How do user's favorite genres affect Streama?",
+				TEXT: "Coming soon..."
+			},
+			USEFUL_LINKS: {
+				TITLE: "Useful links",
+				TEXT: "Coming soon..."
+			},
 		}
 	});
 });
