4bb13439257b5ee7e71056b81eb6b12794bf7d7a
dularion <antonia.hulha@systaro.de>
2017-12-09 15:02:05 +0100

    rewrite admin movies ctrl with hoisted functions

--- grails-app/assets/javascripts/streama/controllers/admin-movies-ctrl.js
+++ grails-app/assets/javascripts/streama/controllers/admin-movies-ctrl.js
@@ -10,60 +10,69 @@ angular.module('streama').controller('adminMoviesCtrl', [
   vm.searchText = "Search Movie from collection or TheMovieDB...";
 
   vm.createFromFiles = createFromFiles;
+  vm.openMovieModal = openMovieModal;
+  vm.doSearch = doSearch;
+  vm.alreadyAdded = alreadyAdded;
+  vm.addFromSuggested = addFromSuggested;
+
+  init();
+
+  function init() {
+    apiService.theMovieDb.hasKey().success(function (data) {
+      if (!data.key) {
+        vm.hasMovieDBKey = false;
+        vm.searchText = "Search Movie from collection...";
+      }
+    });
+
+    apiService.movie.list().success(function (data) {
+      vm.movies = data;
+      vm.loading = false;
+    });
+  }
 
 
-  apiService.theMovieDb.hasKey().success(function (data) {
-    if (!data.key) {
-      vm.hasMovieDBKey = false;
-      vm.searchText = "Search Movie from collection...";
-    }
-  });
+  function openMovieModal() {
+    modalService.movieModal(null, function (data) {
+      $state.go('admin.movie', {movieId: data.id});
+    });
+  }
 
-	apiService.movie.list().success(function (data) {
-		vm.movies = data;
-		vm.loading = false;
-	});
-
-	vm.openMovieModal = function () {
-		modalService.movieModal(null, function (data) {
-			$state.go('admin.movie', {movieId: data.id});
-		});
-	};
-
-	vm.doSearch = function (query) {
+  function doSearch(query) {
     if (vm.hasMovieDBKey) {
       return apiService.theMovieDb.search('movie', query).then(function (data) {
         vm.suggestedMovies = data.data;
       });
     }
-	};
+  }
 
-	vm.addFromSuggested = function (movie, redirect) {
-		var tempMovie = angular.copy(movie);
-		var apiId = tempMovie.id;
-		delete tempMovie.id;
-		tempMovie.apiId = apiId;
+  function addFromSuggested(movie, redirect) {
+    var tempMovie = angular.copy(movie);
+    var apiId = tempMovie.id;
+    delete tempMovie.id;
+    tempMovie.apiId = apiId;
 
-		apiService.movie.save(tempMovie).success(function (data) {
-			if(redirect){
-				$state.go('admin.movie', {movieId: data.id});
-			}else{
-				vm.movies.push(data);
-			}
-		});
-	};
+    apiService.movie.save(tempMovie).success(function (data) {
+      if(redirect){
+        $state.go('admin.movie', {movieId: data.id});
+      }else{
+        vm.movies.push(data);
+      }
+    });
+  }
 
-	vm.alreadyAdded = function (movie) {
-		console.log('%c movie', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;', movie);
-		return movie.id && _.find(vm.movies, {apiId: movie.id.toString()});
-	};
+  function alreadyAdded(movie) {
+    console.log('%c movie', 'color: deeppink; font-weight: bold; text-shadow: 0 0 5px deeppink;', movie);
+    return movie.id && _.find(vm.movies, {apiId: movie.id.toString()});
+  }
 
-	function createFromFiles() {
-		modalService.createFromFilesModal('movie').then(function (data) {
-			apiService.movie.list().success(function (data) {
-				angular.extend(vm.movies, data);
-			});
-		});
-	}
+
+  function createFromFiles() {
+    modalService.createFromFilesModal('movie').then(function (data) {
+      apiService.movie.list().success(function (data) {
+        angular.extend(vm.movies, data);
+      });
+    });
+  }
 
 }]);
