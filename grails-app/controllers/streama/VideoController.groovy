c7e976d48bf394c13de5d7fe0dd02683eb88fa16
Justin Hulha <justin.hulha@systaro.de>
2017-12-20 15:23:47 +0100

    add resolve/unresolve functions for individual reports and make multiple report resolving only work with unresolved reports

--- grails-app/controllers/streama/VideoController.groovy
+++ grails-app/controllers/streama/VideoController.groovy
@@ -245,7 +245,29 @@ class VideoController {
   }
 
   @Transactional
-  def resolveReports() {
+  def resolveReport() {
+    def jsonData = request.JSON
+    def report = Report.get(jsonData.reportId)
+    report.resolved = true
+    report.lastUpdated = new Date()
+    report.save()
+
+    respond report
+  }
+
+  @Transactional
+  def unresolveReport() {
+    def jsonData = request.JSON
+    def report = Report.get(jsonData.reportId)
+    report.resolved = false
+    report.lastUpdated = new Date()
+    report.save()
+
+    respond report
+  }
+
+  @Transactional
+  def resolveMultipleReports() {
     def jsonData = request.JSON
     def resolvedReports = []
     jsonData.ids.each { id ->
@@ -253,7 +275,10 @@ class VideoController {
       report.resolved = true
       report.lastUpdated = new Date()
       report.save()
-      resolvedReports.add(id)
+      resolvedReports.add([
+        id:id,
+        resolved:report.resolved
+      ])
     }
     respond resolvedReports
   }
