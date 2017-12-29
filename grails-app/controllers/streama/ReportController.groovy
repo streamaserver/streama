464eda40fb0e8f6dca550552325784f7e594b977
Justin Hulha <justin.hulha@systaro.de>
2017-12-29 15:12:27 +0100

    clean up code/ remove unused gsons

--- grails-app/controllers/streama/ReportController.groovy
+++ grails-app/controllers/streama/ReportController.groovy
@@ -12,7 +12,6 @@ import grails.transaction.Transactional
 class ReportController {
 
   static responseFormats = ['json', 'xml']
-//  static allowedMethods = [save: "PUT"]
 
   def springSecurityService
 
