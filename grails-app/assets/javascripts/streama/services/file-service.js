c5f1feb660a68cdb9a508dcd157a0d49b646f1f0
Jeronimo95 <2177618+Jeronimo95@users.noreply.github.com>
2018-01-07 23:07:27 +1100

    Fix issue where asset settings weren't loading on non-settings pages

--- grails-app/assets/javascripts/streama/services/file-service.js
+++ grails-app/assets/javascripts/streama/services/file-service.js
@@ -3,18 +3,30 @@
 angular.module('streama').factory('fileService',[ 'apiService', '$sce' , function ( apiService, $sce) {
   return {
     getAssetFromSetting: function (setting) {
-
+      if(typeof setting === "undefined")return false;
       var assetURL = setting.value;
 
-      if(assetURL.startsWith("upload:")){
-        var id = assetURL.split(":")[1]
-        apiService.file.getURL(id)
-          .success(function (data) {  setting.src = data.url})
+      if(assetURL !== setting.prevValue) {
+        //we do this so we don't get caught in an infinite loop if the value doesn't need to be changed
+        setting.prevValue = assetURL;
+
+        if (assetURL.startsWith("upload:")) {
+
+          var id = assetURL.split(":")[1];
+          apiService.file.getURL(id)
+            .success(function (data) {
+              setting.src = data.url
+              return true;
+            })
+
+        } else {
+          setting.src = assetURL;
+          return true;
+        }
 
       }else{
-        setting.src = assetURL;
+        return true;
       }
-
 
     }
   };
