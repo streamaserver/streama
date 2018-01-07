f444cfbeeddef4f3c33da25720516f861aec6749
dularion <antonia.hulha@systaro.de>
2018-01-07 14:28:11 +0100

    add alertify box for "session expired" to login page if param is present with english translation

--- grails-app/assets/javascripts/streama/translations/EN_us.js
+++ grails-app/assets/javascripts/streama/translations/EN_us.js
@@ -9,7 +9,8 @@ angular.module('streama.translations').config(function ($translateProvider) {
 			USERNAME: 'Username',
 			PASSWORD: 'Password',
 			FIRST_TIME_HINT: 'First time logging in? Try \'admin\' for both fields.',
-			SUBMIT: 'Login'
+			SUBMIT: 'Login',
+      SESSION_EXPIRED: 'Your session expired since your last activity. Please login again.'
 		},
 		DASHBOARD: {
 			TITLE: 'Dashboard',
