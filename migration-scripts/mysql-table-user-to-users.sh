fcebcf3c99cfc38b582ecdf87a6f4e08c1bfcac2
DerVerruckteFuchs <cbillhei@gmu.edu>
2018-07-15 11:31:10 -0400

    add mysql table migration script

---
+++ migration-scripts/mysql-table-user-to-users.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# get streama database user name, password and DB name for mysql
+echo input mysql user name for streama database:
+read -s USER
+echo ''
+echo input mysql user password for streama database:
+read -s PASS
+echo ''
+echo input mysql database name for streama database:
+read -s D
+echo ''
+
+# check for 'user'/'users' table
+if [ -z "$(mysql -u $USER --password=$PASS -D $DB -e "show tables like 'user'")" ]; then
+	echo "'user' table does not exist"
+	if [ -z "$(mysql -u $USER --password=$PASS -D $DB -e "show tables like 'users'")" ]; then
+		echo "'users' table does not exist either"
+		echo "there is an issue with your streama database"
+	else
+		echo "'users table exists'"
+		echo "no need to rename table"
+	fi
+
+else
+	echo "'user' table exists"
+	echo "renaming 'user' table to 'users'"
+	mysql -u $USER --password=$PASS -D $DB -e "ALTER TABLE user RENAME users"
+        if [ -z "$(mysql -u $USER --password=$PASS -D $DB -e "show tables like 'users'")" ]; then
+		echo "rename failed"
+	else
+		echo "rename succeeded"
+	fi
+
+fi
+
+echo ""
+echo "finished"
