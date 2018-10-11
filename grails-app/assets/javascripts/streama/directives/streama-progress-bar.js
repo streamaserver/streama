a04ae15666c6fd0b1e900a7f9d78b6043ef10b60
dularion <antonia.hulha@systaro.de>
2018-10-11 21:51:58 +0200

    add directive streama-progress-bar.js

---
+++ grails-app/assets/javascripts/streama/directives/streama-progress-bar.js
@@ -0,0 +1,16 @@
+//= wrapped
+
+angular.module('streama').directive('streamaProgressBar', function () {
+  return {
+    restrict: 'E',
+    templateUrl: '/streama/directive--streama-progress-bar.htm',
+    scope: {
+      video: '='
+    },
+    link: function ($scope, $elem, $attrs) {
+      // $scope.videoObject = angular.merge({}, $scope.video);
+      // $scope.videoObject.currentPlayTime = $scope.video.currentPlayTime || $scope.runtime;
+      // $scope.videoObject.runtime = $scope.video.runtime || $scope.total;
+    }
+  }
+});
