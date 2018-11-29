42d54b5a87c517b3153839fc789ffa7a91524d47
dularion <antonia.hulha@systaro.de>
2018-11-29 19:54:10 +0100

    respect profile Language when switching profiles

--- grails-app/assets/javascripts/streama/services/profile-service.js
+++ grails-app/assets/javascripts/streama/services/profile-service.js
@@ -1,11 +1,12 @@
 'use strict';
 
-angular.module('streama').factory('profileService', function (localStorageService, apiService, $state, $rootScope) {
+angular.module('streama').factory('profileService', function (localStorageService, apiService, $state, $rootScope, $translate) {
 
   function setCurrentProfile(profile) {
     localStorageService.set('currentProfile', profile);
     $rootScope.currentProfile = profile;
     $state.go('dash', {}, {reload: true});
+    $translate.use(_.get($rootScope, 'currentProfile.profileLanguage') || _.get($rootScope, 'currentUser.language') || 'en');
   }
 
   function getCurrentProfile() {
