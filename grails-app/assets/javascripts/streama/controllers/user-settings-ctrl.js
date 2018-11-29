83f3d89e6ab69cde8fbe071f3e61e0e5f49efadf
dularion <antonia.hulha@systaro.de>
2018-11-29 19:59:08 +0100

    rename profileCtrl & profile route to userSettings

--- grails-app/assets/javascripts/streama/controllers/user-settings-ctrl.js
+++ grails-app/assets/javascripts/streama/controllers/user-settings-ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('streama').controller('profileCtrl', function ($scope, apiService, $rootScope, userService) {
+angular.module('streama').controller('userSettingsCtrl', function ($scope, apiService, $rootScope, userService) {
   $scope.user = angular.copy($rootScope.currentUser);
   $scope.loading = true;
   $scope.passwordData = {};
