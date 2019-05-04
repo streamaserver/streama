3c891b2da429f884620119124b70e77921e4720f
dularion <antonia.hulha@systaro.de>
2019-05-04 21:06:34 +0200

    refactor: hoist methods in modal-tvshow-ctrl.js

--- grails-app/assets/javascripts/streama/controllers/modal-tvshow-ctrl.js
+++ grails-app/assets/javascripts/streama/controllers/modal-tvshow-ctrl.js
@@ -8,47 +8,55 @@ angular.module('streama').controller('modalTvShowCtrl', [
   $scope.tvShow = tvShow || {};
   $scope.hasMovieDBKey = true;
 
-  apiService.theMovieDb.hasKey().then(function (data) {
-    if (!data.data.key) {
-      $scope.tvShow.manualInput = true;
-      $scope.hasMovieDBKey = false;
-    }
-  });
+	$scope.cancel = cancel;
+	$scope.saveShow = saveShow;
+	$scope.selectFromAPI = selectFromAPI;
+	$scope.search = search;
+	$scope.toggleAddManually = toggleAddManually;
 
-	$scope.toggleAddManually = function () {
+	init();
+
+  function init(){
+		apiService.theMovieDb.hasKey().then(function (data) {
+			if (!data.data.key) {
+				$scope.tvShow.manualInput = true;
+				$scope.hasMovieDBKey = false;
+			}
+		});
+
+		setTimeout(function () {
+			$('.name-input').focus();
+		}, 200);
+	}
+
+	function toggleAddManually() {
 		$scope.tvShow.manualInput = !$scope.tvShow.manualInput;
-  };
+	}
 
-	$scope.saveShow = function (video) {
+	function saveShow(video) {
 		apiService.tvShow.save(video).then(function (data) {
 			$uibModalInstance.close(data.data);
-      alertify.success("TV Show saved.");
+			alertify.success("TV Show saved.");
 		});
-	};
+	}
 
-	$scope.selectFromAPI = function ($item) {
+	function selectFromAPI($item) {
 		var apiId = $item.id;
 		delete $item.id;
 		$scope.tvShow = $item;
 		$scope.tvShow.apiId = apiId;
 		$scope.hasMovieDBKey = true;
 		$scope.tvShow.manualInput = false;
-	};
+	}
 
-
-	$scope.search = function (query) {
+	function search(query) {
 		return apiService.theMovieDb.search('tv', query).then(function (data) {
 			return data.data;
 		});
-	};
+	}
 
-
-	setTimeout(function () {
-		$('.name-input').focus();
-	}, 200);
-
-
-	$scope.cancel = function () {
+	function cancel() {
 		$uibModalInstance.dismiss('cancel');
-	};
+	}
+
 }]);
