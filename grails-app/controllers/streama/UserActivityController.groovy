4615920a0c6c95340dcdbdbd637cd23a81f7dfa7
dularion <antonia.hulha@systaro.de>
2019-05-30 08:21:10 +0200

    add missing params.sort handling for UserActivityController.groovy #814

--- grails-app/controllers/streama/UserActivityController.groovy
+++ grails-app/controllers/streama/UserActivityController.groovy
@@ -10,7 +10,7 @@ class UserActivityController {
 
     def index(Integer max) {
       params.max = Math.min(max ?: 20, 100)
-      params.sort = 'id'
+      params.sort = params.sort ?: 'id'
       params.order = 'DESC'
       params.offset = params.int('offset') ?: 0
 
