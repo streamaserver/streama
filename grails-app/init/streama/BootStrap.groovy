fdb53ad29d105ff8c8a5cdbc758da663be9ea3f6
dularion <antonia.hulha@systaro.de>
2019-11-03 22:11:45 +0100

    fix problem of broken admin-login after renaming the admin-user

--- grails-app/init/streama/BootStrap.groovy
+++ grails-app/init/streama/BootStrap.groovy
@@ -8,13 +8,15 @@ class BootStrap {
 
     def init = { servletContext ->
         marshallerService.init()
-        defaultDataService.createDefaultRoles()
-        defaultDataService.createDefaultUsers()
-        defaultDataService.createDefaultSettings()
+        if(!Role.findByAuthority("ROLE_ADMIN")){  //this assumes that first-time init is done
+          defaultDataService.createDefaultRoles()
+          defaultDataService.createDefaultUsers()
+        }
 
+        migrationService.importMovieDbGenres()
+        defaultDataService.createDefaultSettings()
         migrationService.setDefaultDeletedFlag()
         migrationService.setTrailerForMovies()
-        migrationService.importMovieDbGenres()
         migrationService.addGenresToMoviesAndShows()
         migrationService.setTheMovieDBKey()
         migrationService.fixLogoValue()   //2017-03-04
