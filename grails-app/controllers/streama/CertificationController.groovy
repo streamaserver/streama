a6c425cb8dc70d8a952895f30aa799265caa5432
dularion <antonia.hulha@systaro.de>
2021-03-05 23:41:48 +0100

    add certification Administration & import

---
+++ grails-app/controllers/streama/CertificationController.groovy
@@ -0,0 +1,95 @@
+package streama
+
+
+import grails.transaction.Transactional
+
+import static org.springframework.http.HttpStatus.*
+
+@Transactional(readOnly = true)
+class CertificationController {
+
+    def theMovieDbService
+
+    static responseFormats = ['json', 'xml']
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = 999
+        respond Certification.list(params), [status: OK]
+    }
+
+    @Transactional
+    def save() {
+        def data = request.JSON
+        Certification certificationInstance = data.id ? Certification.get(data.id) : new Certification()
+
+        certificationInstance.properties = data
+        certificationInstance.validate()
+        if (certificationInstance.hasErrors()) {
+            render status: NOT_ACCEPTABLE
+            return
+        }
+
+        certificationInstance.save flush:true
+        respond certificationInstance, [status: CREATED]
+    }
+
+    @Transactional
+    def importCertifications() {
+      String type = params.type
+      def certifications = theMovieDbService.listCertifications(type)
+      List<Certification> certificationResult = []
+
+      certifications.each{ certData ->
+        if(Certification.findByCertificationAndType(certData.certification, type)){
+          return
+        }
+        Certification certification = new Certification()
+        certification.certification = certData.certification
+        certification.description = certData.meaning
+        certification.type = type
+        certification.save()
+
+        certificationResult.add(certification)
+      }
+
+      respond certificationResult
+    }
+
+    @Transactional
+    def update(Certification certificationInstance) {
+        if (certificationInstance == null) {
+            render status: NOT_FOUND
+            return
+        }
+
+        certificationInstance.validate()
+        if (certificationInstance.hasErrors()) {
+            render status: NOT_ACCEPTABLE
+            return
+        }
+
+        certificationInstance.save flush:true
+        respond certificationInstance, [status: OK]
+    }
+
+    @Transactional
+    def delete(Certification certificationInstance) {
+
+        if (certificationInstance == null) {
+            render status: NOT_FOUND
+            return
+        }
+        if (certificationInstance.apiId) {
+            render status: PRECONDITION_FAILED
+            return
+        }
+
+        certificationInstance.delete flush:true
+        render status: NO_CONTENT
+    }
+
+    def show(Certification certificationInstance) {
+        respond certificationInstance, [status: OK]
+    }
+}
