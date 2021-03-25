da52465e17371b8601a9b707345aa2ed5761b695
Likh-Alex <likhachov991@protonmail.com>
2021-03-25 13:38:20 +0200

    JShint changes

--- grails-app/assets/javascripts/streama/controllers/dash-ctrl.js
+++ grails-app/assets/javascripts/streama/controllers/dash-ctrl.js
@@ -29,8 +29,8 @@ angular.module('streama').controller('dashCtrl',
             var data = response.data;
             localStorageService.set('currentProfile', data[0]);
             initMedia();
-          }
-        , function (data) {
+          },
+          function (data) {
           alertify.error(data.message);
         });
       } else {
@@ -39,19 +39,19 @@ angular.module('streama').controller('dashCtrl',
     }
 
     function initMedia() {
-      if(isDashType("home") || isDashType("discover-movies")){
+      if(isDashType('home') || isDashType('discover-movies')){
         vm.movie = mediaListService.init(apiService.dash.listMovies, {sort: 'title', order: 'ASC'}, currentUser);
       }
-      if(isDashType("home") || isDashType("discover-shows")){
+      if(isDashType('home') || isDashType('discover-shows')){
         vm.tvShow = mediaListService.init(apiService.dash.listShows, {sort: 'name', order: 'ASC'}, currentUser);
       }
-      if(isDashType("home") || isDashType("continue-watching")){
-        vm.continueWatching = mediaListService.init(apiService.dash.listContinueWatching, {sort: 'lastUpdated', order: 'DESC'}, currentUser)
+      if(isDashType('home') || isDashType('continue-watching')){
+        vm.continueWatching = mediaListService.init(apiService.dash.listContinueWatching, {sort: 'lastUpdated', order: 'DESC'}, currentUser);
       }
-      if(isDashType("home") || isDashType("watchlist")){
+      if(isDashType('home') || isDashType('watchlist')){
         vm.watchlistEntry = mediaListService.init(apiService.watchlistEntry.list, {sort: 'id', order: 'DESC'}, currentUser);
       }
-      if(isDashType("home")){
+      if(isDashType('home')){
         vm.genericVideo = mediaListService.init(apiService.dash.listGenericVideos, {sort: 'title', order: 'ASC'}, currentUser);
         apiService.dash.listNewReleases().then(onNewReleasesLoaded);
         apiService.dash.listRecommendations().then(onRecommendedLoaded);
@@ -136,10 +136,10 @@ angular.module('streama').controller('dashCtrl',
 
     function handleWatchlistUpdate(action, item){
       switch (action) {
-        case "added":
+        case 'added':
           addToWatchlist(item);
           break;
-        case "removed":
+        case 'removed':
           removeFromWatchlist(item);
           break;
       }
@@ -148,17 +148,17 @@ angular.module('streama').controller('dashCtrl',
     function addToWatchlist(item) {
       apiService.watchlistEntry.create(item).then(function (response) {
         vm.watchlistEntry.list = vm.watchlistEntry.list ? vm.watchlistEntry.list : [];
-        updateWatchlist("added", _.get(vm.watchlistEntry, 'list'), item, response.data);
+        updateWatchlist('added', _.get(vm.watchlistEntry, 'list'), item, response.data);
       });
     }
 
     function removeFromWatchlist(item) {
       vm.watchlistEntry.list = vm.watchlistEntry.list ? vm.watchlistEntry.list : [];
-      alertify.set({buttonReverse: true, labels: {ok: "Yes", cancel: "Cancel"}});
-      alertify.confirm("Are you sure you want to remove this video from your watchlist?", function (confirmed) {
+      alertify.set({buttonReverse: true, labels: {ok: 'Yes', cancel: 'Cancel'}});
+      alertify.confirm('Are you sure you want to remove this video from your watchlist?', function (confirmed) {
         if (confirmed) {
           apiService.watchlistEntry.delete(item).then(function (response) {
-            updateWatchlist("removed", _.get(vm.watchlistEntry, 'list'), item);
+            updateWatchlist('removed', _.get(vm.watchlistEntry, 'list'), item);
           });
         }
       });
@@ -173,27 +173,27 @@ angular.module('streama').controller('dashCtrl',
         removeMediaFromList(list, media);
         alertify.success('The '+type+' was removed from your watchlist.');
       }
-      list.sort(function(a,b) { return (a.id < b.id) ? 1 : ((a.id > b.id) ? -1 : 0)});
+      list.sort(function(a,b) { return (a.id < b.id) ? 1 : ((a.id > b.id) ? -1 : 0);});
     }
 
     function removeMediaFromList(list, media){
       _.remove(list, function (watchlistEntry) {
-        return (watchlistEntry.video ? watchlistEntry.video.id : watchlistEntry.tvShow.id) === media.id
+        return (watchlistEntry.video ? watchlistEntry.video.id : watchlistEntry.tvShow.id) === media.id;
       });
     }
 
     function handleVideoListsUpdate(media){
       var type = media.mediaType;
       switch (type) {
-        case "tvShow":
+        case 'tvShow':
           watchlistStatusHandler(vm.tvShow.list, media);
-          type = "show";
+          type = 'show';
           break;
-        case "movie":
+        case 'movie':
           watchlistStatusHandler(vm.movie.list, media);
           type = 'movie';
           break;
-        case "genericVideo":
+        case 'genericVideo':
           watchlistStatusHandler(vm.genericVideo.list, media);
           type = 'video';
           break;
@@ -202,13 +202,13 @@ angular.module('streama').controller('dashCtrl',
       }
       watchlistStatusHandler(vm.newReleases, media);
       watchlistStatusHandler(vm.continueWatching, media);
-      return type
+      return type;
     }
 
     function watchlistStatusHandler(mediaList, item){
-      var index = _.findIndex(mediaList, function(element) { return item.id === element.id});
+      var index = _.findIndex(mediaList, function(element) { return item.id === element.id;});
       if(index > 0){
-        mediaList[index].inWatchlist = !mediaList[index].inWatchlist
+        mediaList[index].inWatchlist = !mediaList[index].inWatchlist;
       }
     }
 
